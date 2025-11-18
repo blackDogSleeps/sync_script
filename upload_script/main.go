01cb2efdba11c3d70269633176eb0a9acd1cd385
Vasiliy Kuryshev <v.kuryshev@start2play.ru>
2025-11-18 20:32:06 +0300

    some progress on upload_script

--- upload_script/main.go
+++ upload_script/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"fmt"
 	"os"
+	"os/exec"
 	"slices"
 )
 
@@ -54,23 +56,18 @@ func getLocalEnvVariables() map[string]string {
 		}
 	}
 
-	var errors []string;
+	var errors []string
+
 	if len(userName) < 1 {
 		errors = append(errors, "Не указан SERVER_USER_NAME в .env")
-		//fmt.Println("Не указан SERVER_USER_NAME в .env")
-		//os.Exit(0)
 	}
 
 	if len(pubKeyPath) < 1 {
 		errors = append(errors, "Не указан SERVER_PUB_KEY_PATH в .env")
-		//fmt.Println("Не указан SERVER_PUB_KEY_PATH в .env")
-		//os.Exit(0)
 	}
 
 	if len(appName) < 1 {
 		errors = append(errors, "Не указан SERVER_APP_NAME в .env")
-		//fmt.Println("Не указан SERVER_APP_NAME в .env")
-		//os.Exit(0)
 	}
 
 	if len(errors) > 0 {
@@ -87,26 +84,74 @@ func getLocalEnvVariables() map[string]string {
 	}
 }
 
+func execCommand(userName string, serverCommands string) {
+	serverLogin := fmt.Sprintf("%s@88.99.160.4", userName)
+	fmt.Println(serverLogin)
+	ctx := context.Background()
+	cmd := exec.CommandContext(ctx, "ssh", serverLogin, "-p", "58533", serverCommands)
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	sshErr := cmd.Run()
+	if sshErr != nil {
+		fmt.Println("ssh command didn't fly: ", sshErr)
+		os.Exit(0)
+	}
+}
+
+func getUserInput() {
+	var reply string
+
+	fmt.Println("Удалить их и загрузить новые (y/n)?")
+
+	for reply != "y" {
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+		scanErr := scanner.Err()
+
+		if scanErr != nil {
+			fmt.Println("Something wrong with scanner: ", scanErr)
+			os.Exit(0)
+		}
+
+		reply = scanner.Text()
+
+		if reply == "n" {
+			fmt.Println("Возвращайтесь когда будете готовы")
+			os.Exit(0)
+		}
+
+		if reply != "y" && reply != "n" {
+			fmt.Println("Можно выбрать только 'y' или 'n' =)")
+		}
+	}
+
+	fmt.Println("Аминь!")
+}
+
 func main() {
 	envVars := getLocalEnvVariables()
-	
-	fmt.Println(envVars);
+	appPath := fmt.Sprintf("/var/www/projects/app/%s", envVars["appName"])
+	serverCommands := fmt.Sprintf("cd %s/ && pwd && ls", appPath)
 
-	 
-	//if [[ ! $SERVER_USER_NAME ]]
-	//then
-	//    echo "Не указан SERVER_USER_NAME в .env "
-	//    exit
-	//fi
-	//if [[ ! $SERVER_PUB_KEY_PATH ]]
-	//then
-	//    echo "Не указан SERVER_PUB_KEY_PATH в .env "
-	//    exit
-	//fi
-	//if [[ ! $SERVER_APP_NAME ]]
-	//then
-	//    echo "Не указан SERVER_APP_NAME в .env "
-	//    exit
-	//fi
+	fmt.Println("Параметры из .env.local")
+	fmt.Printf("SERVER_USER_NAME=%s\n", envVars["userName"])
+	fmt.Printf("SERVER_PUB_KEY_PATH=%s\n", envVars["pubKeyPath"])
+	fmt.Printf("SERVER_APP_NAM=%s\n", envVars["appName"])
+	fmt.Printf("appPath: %s\n", appPath)
+	fmt.Println("Файлы на сервере:")
+	fmt.Println("=======")
+
+	// The ssh command we're translating:
+	//ssh $SERVER_USER_NAME@88.99.160.4 -p 58533 'cd '$_appPath'/ && pwd && ls'
+	execCommand(envVars["userName"], serverCommands)
+	getUserInput()
+
+	fmt.Println("Удаляем файлы")
+	//ssh $SERVER_USER_NAME@88.99.160.4 -p 58533 'rm -rf '$_appPath'/* '$_appPath'/.htaccess'
+	serverCommands = fmt.Sprintf("rm -rf %s/* %s/.htaccess", appPath)
+	//execCommand(envVars["userName"], serverCommands)
 
 }
